ba8d62028fabd5268ed5d337079ddc301eb4105f
Dkriss <89033170+Dkriss@users.noreply.github.com>
2023-12-11 08:53:36 +0700

    ngrapihno sing double ndek deklarasi main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/get_instance.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
 import 'package:mobilepraktikum/controller/notification_handler.dart';
 import 'package:mobilepraktikum/firebase_options.dart';
 import 'package:mobilepraktikum/view/welcome_screen/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 void main() async {
-  /////////////// API FIREBASE////////////////////
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-  /////////////// FUNGSI INIT PUSH NOTIFIKASI ///////////////////////////
+
   await Get.putAsync(() async => await SharedPreferences.getInstance());
   await FirebaseMessagingHandler().initPushNotification();
   await FirebaseMessagingHandler().initLocalNotification();
 
-
-  runApp(const GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: WelcomeScreen(),
-  ));
+  runApp(const MyApp());
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: WelcomeScreen());
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+    );
   }
 }
